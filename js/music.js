a80c2f92eb1dafc20c6788b79f81ab5cc5959b0c
fshby <108744896+fshby@users.noreply.github.com>
2022-09-19 09:35:13 +0800

    Add files via upload

--- js/music.js
+++ js/music.js
@@ -1,18 +1,23 @@
 
 let bmusic=document.getElementById("bb");
 let picture=document.getElementById("t1");
+let xingxing1=document.getElementById("x1");
 console.log(bb,typeof bmusic);
 console.log(t1,typeof picture);
 let i = true;
 picture.onclick = function(){
     if(i){
         bmusic.play();
+        picture.style.animationPlayState="running";
+        xingxing1.style.animationPlayState="running";
+       
         picture.src="etet.gif";
         i = false;
     }else{
         bmusic.pause();
         i = true;
         picture.src="etet.png";
+        picture.style.animationPlayState="paused"
         
 
     }
